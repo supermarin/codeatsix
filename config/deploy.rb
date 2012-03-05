99c54ffb0744818d7168a073c114ba9f08e87c36
Nikica Jokic <nikica.jokic@infinum.hr>
2012-03-05 13:30:41 +0100

    More deploy script fixes.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,21 @@
+host = "codeatsix.infinum.hr"
+
 set :application, "codeatsix.infinum.hr"
 set :repository,  "git@github.com:neektza/codeatsix.git"
+set :deploy_to, "/var/www/c/codeatsix.infinum.hr" 
 
 set :scm, :git
-set :user "www-data"
+set :user, "www-data"
+set :use_sudo, false
 
 set :branch, "master"
 set :deploy_via, :remote_cache
 
 ssh_options[:forward_agent] = true
 
-role :web, "vz3.infinum.hr"
-role :app, "vz3.infinum.hr"
-role :db,  "mysql.infinum.hr", :primary => true
+role :web, host
+role :app, host 
+role :db,  host, :primary => true
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
