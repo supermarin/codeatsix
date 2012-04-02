e27b79566dbc5afa9a7320a2a3abeb9de633d7e0
Marin Usalj <mneorr@gmail.com>
2012-04-02 10:50:06 +0200

    tests are passing

--- app/models/submit.rb
+++
@@ -1,6 +0,0 @@
-class Submit < ActiveRecord::Base
-  validates :name,  :presence => true
-  validates :surname, :presence => true
-  validates :email, :presence => true
-  #dodat verifikaciju emaila bolju
-end
